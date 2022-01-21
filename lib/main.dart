73841796392cec49b1f6b827430235f2141ff2ed
InTECHT <88443508+InTECHT@users.noreply.github.com>
2022-01-21 13:07:18 +0500

    Hassan ~ Font Family

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:build_own_cake/app%20screens/get_started.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MyApp());
@@ -8,13 +9,32 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
+  final MaterialColor primaryColor = const MaterialColor(
+    0xffd62764,
+    <int, Color>{
+      50: Color(0xffd62764),
+      100: Color(0xffd62764),
+      200: Color(0xffd62764),
+      300: Color(0xffd62764),
+      400: Color(0xffd62764),
+      500: Color(0xffd62764),
+      600: Color(0xffd62764),
+      700: Color(0xffd62764),
+      800: Color(0xffd62764),
+      900: Color(0xffd62764),
+    },
+  );
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Build Your Own Cake',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: primaryColor,
+        textTheme: GoogleFonts.nunitoTextTheme(
+          Theme.of(context).textTheme,
+        ),
       ),
       home: const GetStarted(),
     );
--- lib/widgets/app_bar.dart
+++ lib/widgets/app_bar.dart
@@ -3,16 +3,21 @@ import 'package:build_own_cake/utils/dynamic_sizes.dart';
 import 'package:build_own_cake/widgets/text_widget.dart';
 import 'package:flutter/material.dart';
 
-
-Widget appBar (context) {
+Widget appBar(context) {
   return Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Icon(
-                  Icons.notes_outlined,
-                  size: dynamicWidth(context, 0.1),
-                ),
-                text(context, "Buildyourowncake", 0.05, myBlack, bold: true),
-              ],
-            );
+    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    children: [
+      Icon(
+        Icons.notes_outlined,
+        size: dynamicWidth(context, 0.1),
+      ),
+      text(
+        context,
+        "BuildYourCake",
+        0.05,
+        myBlack,
+        bold: true,
+      ),
+    ],
+  );
 }
