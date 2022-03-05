66ce56ca647e7c72135b0fbf9400821a5b5dfa3e
Hassan Gujjar <hbsgujjar111h@gmail.com>
2022-03-05 10:44:04 +0500

    Hassan - Fonts

--- lib/widgets/text_widget.dart
+++ lib/widgets/text_widget.dart
@@ -1,6 +1,5 @@
 import 'package:build_own_cake/utils/dynamic_sizes.dart';
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 Widget text(context, text, size, color,
     {bold = false, alignText = TextAlign.start, maxLines = 2, font = false}) {
@@ -8,7 +7,6 @@ Widget text(context, text, size, color,
     text,
     textAlign: alignText,
     maxLines: maxLines,
-    
     overflow: TextOverflow.ellipsis,
     style: font == false
         ? TextStyle(
@@ -16,12 +14,11 @@ Widget text(context, text, size, color,
             fontSize: dynamicWidth(context, size),
             fontWeight: bold == true ? FontWeight.bold : FontWeight.normal,
           )
-        : GoogleFonts.yellowtail(
-            textStyle: TextStyle(
-              color: color,
-              fontSize: dynamicWidth(context, size),
-              fontWeight: bold == true ? FontWeight.bold : FontWeight.normal,
-            ),
+        : TextStyle(
+            color: color,
+            fontFamily: "Champagne",
+            fontSize: dynamicWidth(context, size),
+            fontWeight: bold == true ? FontWeight.bold : FontWeight.normal,
           ),
   );
 }
